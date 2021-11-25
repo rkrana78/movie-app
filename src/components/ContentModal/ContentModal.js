dd586d999fa9545d9a33f8f65c360602a0b0f907
rkrana78 <rashedul.karim7858@gmail.com>
2021-11-25 19:43:06 +0600

    update modal

---
+++ src/components/ContentModal/ContentModal.js
@@ -0,0 +1,136 @@
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import { img_500, unavailable } from '../../config/config';
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import Carousel from "../Carousel/Carousel";
+import './ContentModal.css'
+
+
+// const style = {
+//   position: 'absolute',
+//   top: '50%',
+//   left: '50%',
+//   transform: 'translate(-50%, -50%)',
+//   width: 400,
+//   bgcolor: 'background.paper',
+//   border: '2px solid #000',
+//   boxShadow: 24,
+//   p: 4,
+// };
+
+const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    paper: {
+        width: "90%",
+        height: "80%",
+        backgroundColor: "#39445a",
+        border: "1px solid #282c34",
+        borderRadius: 10,
+        color: "white",
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(1, 1, 3),
+       
+    },
+}));
+
+
+export default function ContentModal({ children, media_type, id }) {
+    const classes = useStyles();
+    const [open, setOpen] = useState(false);
+    const [contents, setContents] = useState();
+    const [video, setVideo] = useState();
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    useEffect(() => {
+        const url = `https://api.themoviedb.org/3/${media_type}/${id}?api_key=c1b30049dd035636e32a9002d9816015&language=en-US`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setContents(data)
+            })
+
+    }, [id, media_type])
+
+    useEffect(() => {
+        const url = `https://api.themoviedb.org/3/${media_type}/${id}/videos?api_key=c1b30049dd035636e32a9002d9816015&language=en-US`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setVideo(data.results[0]?.key)
+            })
+    }, [id, media_type])
+
+    return (
+        <div>
+            <div className="media" onClick={handleOpen}>{children}</div>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                    {contents && (
+                        <Box className={classes.paper}>
+                            <div className="ContentModal">
+                                <img className="ContentModal__portrait" src={contents.poster_path ? `${img_500}/${contents.poster_path}` : unavailable} alt={contents.name || contents.title} />
+                                <img className="ContentModal__landscape" src={contents.backdrop_path ? `${img_500}/${contents.poster_path}` : unavailable} alt={contents.name || contents.title} />
+                                <div className="ContentModal__about">
+                                    <span className="ContentModal__title">
+                                        {contents.name || contents.title} (
+                                        {(
+                                            contents.first_air_date ||
+                                            contents.release_date ||
+                                            "-----"
+                                        ).substring(0, 4)}
+                                        )
+                                    </span>
+                                    {contents.tagline && (
+                                        <i className="tagline">{contents.tagline}</i>
+                                    )}
+
+                                    <span className="ContentModal__description">
+                                        {contents.overview}
+                                    </span>
+
+                                    <div>
+                                        <Carousel id={id} media_type={media_type} />
+                                    </div>
+
+                                    <Button
+                                        variant="contained"
+                                        startIcon={<YouTubeIcon />}
+                                        color="secondary"
+                                        target="__blank"
+                                        href={`https://www.youtube.com/watch?v=${video}`}
+                                    >
+                                        Watch the Trailer
+                                    </Button>
+                                </div>
+                            </div>
+                        </Box>
+                    )}
+                </Fade>
+            </Modal>
+        </div>
+    );
+}
--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,12 +1,13 @@
 import { Badge } from '@material-ui/core';
 import React from 'react';
 import {img_300, unavailable} from '../../config/config'
+import ContentModal from '../ContentModal/ContentModal';
 import './SingleContent.css'
 
 const SingleContent = (props) => {
     const{title, id, poster_path, name, first_air_date ,release_date, media_type, vote_average} = props.content;
     return (
-        <div className="media">
+        <ContentModal media_type={media_type} id={id}>
             <Badge badgeContent={vote_average} color={vote_average > 6 ? "primary" : "secondary"}/>
            <img className="poster" src={ poster_path ?`${img_300}/${poster_path}` : unavailable} alt={title} />
            <b className="title">{title || name}</b>
@@ -17,7 +18,7 @@ const SingleContent = (props) => {
                } */}
                <span className="subTitle">{first_air_date || release_date}</span>
            </span>
-        </div>
+        </ContentModal>
     );
 };
 
